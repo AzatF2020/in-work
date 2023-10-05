133a4dbf7bfcff5ac780df798d8d8481f94a2bb0
Азат Гайнутдинов <azatgajnutdinov@MacBook-Air-Azat.local>
2023-10-05 17:53:11 +0300

    created blocks header, intro, about

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,8 +1,9 @@
 import "virtual:svg-icons-register";
 import "../scss/style.scss";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+});
 
 window.addEventListener("load", () => {
-  document.body.classList.add("loaded");
+
 });
