38a0d1d9ac9a6689b3214342de9df8fa068b6931
Азат Гайнутдинов <azatgajnutdinov@MacBook-Air-Azat.local>
2023-10-05 20:36:55 +0300

    created animation for intro

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,6 +1,8 @@
 import "virtual:svg-icons-register";
+import InitIntroSplitText from "./introSplitText.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
+  InitIntroSplitText()
 });
 
 window.addEventListener("load", () => {
