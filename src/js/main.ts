0716ddc977f34bf8e2d3f334c652bde5379b23e5
Азат Гайнутдинов <azatgajnutdinov@MacBook-Air-Azat.local>
2023-10-09 14:22:32 +0300

    created burger modal

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,13 +4,15 @@ import elements from "./elements.ts"
 import process from "./process.ts";
 import industry from "./industry.ts";
 import clients from "./clients.ts";
+import burgerModal from "./burger-modal.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
   InitIntroSplitText()
   process()
   industry()
   clients()
-  // elements()
+  burgerModal()
+  elements()
 });
 
 window.addEventListener("load", () => {
