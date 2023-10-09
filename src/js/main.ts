3b6ecf125da857021784bd933b9c854de9a0c2f0
Азат Гайнутдинов <azatgajnutdinov@MacBook-Air-Azat.local>
2023-10-09 11:50:45 +0300

    created adaptive for advantages, intro, header, about

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,8 +1,10 @@
 import "virtual:svg-icons-register";
 import InitIntroSplitText from "./introSplitText.ts";
+import elements from "./elements.ts"
 
 document.addEventListener("DOMContentLoaded", () => {
   InitIntroSplitText()
+  elements()
 });
 
 window.addEventListener("load", () => {
